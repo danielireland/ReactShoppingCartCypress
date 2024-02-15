4b17bc41f9b26a7ffd79fa89204caad9511a5a35
Daniel Ireland <157131901+danielireland@users.noreply.github.com>
2024-02-15 15:32:31 +0000

    Add additional test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,10 +7,15 @@ const addToCartBtn = 'button:contains("Add to cart")';
 const cartPriceText = '[class="Cart__SubPriceValue-sc-1h98xa9-9 dCNBgZ"]';
 const cartProductName = '[class="CartProduct__Title-sc-11uohgb-2 jVsMjn"]';
 const cartMenuIcon = '[class="Cart__CartButton-sc-1h98xa9-0 Xqoyz"]';
-const checkoutBtn = 'button:contains("Add to cart")';
+const checkoutBtn = '[class="Cart__CheckoutButton-sc-1h98xa9-11 elxCN"]';
 const subtotalLabel = '[class="Cart__Sub-sc-1h98xa9-7 exHAGw"]';
 const cartHeader = '[class="Cart__HeaderTitle-sc-1h98xa9-6 iLEImJ"]';
 const cartItemTotal = '[class="Cart__CartIcon-sc-1h98xa9-2 fJGXHr"]';
+const increaseItemQuantity = 'button:contains("+")';
+const decreaseItemQuantity = 'button:contains("-")';
+const itemSizeQuantity = '[class="CartProduct__Desc-sc-11uohgb-3 bRgwLG"]';
+const productContainer = '[class="CartProduct__Container-sc-11uohgb-0 iFEDVZ"]';
+const removeProduct = '[title="remove product from cart"]';
 
 const valueRegex = /[$,\s]/g 
 
@@ -24,27 +29,66 @@ describe('Validate opening and closing the checkout menu', () => {
 
   it('Open and close the cart', () => {
 
+    // Open cart and expect elements to be visible
     cy.get(cartMenuIcon).click();
 
     cy.get(checkoutBtn).should('be.visible');
     cy.get(subtotalLabel).should('be.visible');
     cy.get(cartHeader).should('be.visible');
 
+    // Close cart and expect elements to not be visible
     cy.get(cartMenuIcon).click();
 
-    // cy.get(checkoutBtn).should('not.exist'); 
+    cy.get(checkoutBtn).should('not.exist'); 
     cy.get(subtotalLabel).should('not.exist');
     cy.get(cartHeader).should('not.exist');
     
   });
 
+});
+
+describe('Verify product quantity can be updated in the shopping cart', () => {
+
+  it('Add product to cart and update quantity', () => {
+
+    // Add product to cart and store the cart product container
+    cy.get(`[alt="${productNames[0]}"]`).parent().as('product');
+    cy.get('@product').find(addToCartBtn).click();
+
+    cy.get(productContainer).contains(`${productNames[0]}`).parent().as('productCart');
+
+    cy.get('@productCart').then($cartProduct => {
+      expect($cartProduct.text()).to.contain("Quantity: 1");
+    });
+
+    // Increase item quantity and expect quantity count to increase
+    cy.get(increaseItemQuantity).click()
+
+    cy.get('@productCart').then($cartProduct => {
+      expect($cartProduct.text()).to.contain("Quantity: 2");
+    });
+
+    // Decrease item quantity and expect quantity count to increase
+    cy.get(decreaseItemQuantity).click()
+
+    cy.get('@productCart').then($cartProduct => {
+      expect($cartProduct.text()).to.contain("Quantity: 1");
+    });
+
+    // Remove product and expect product to not exist 
+    cy.get('@productCart').parent().find(removeProduct).click()
+
+    cy.get(productContainer).should('not.exist');
+
+  });
 
 });
 
 productNames.forEach(productName => {
   describe(`Validate adding single a product "${productName}" to the cart`, () => {
 
-    it('Add sinlge product to cart and validate', () => {
+    it('Add single product to cart and validate', () => {
+
       cy.get(`[alt="${productName}"]`).parent().as('product');
 
       // Get the value of the product
@@ -52,10 +96,9 @@ productNames.forEach(productName => {
         const productValue = $productValue.text().replace(valueRegex, '');
         cy.get('@product').find(addToCartBtn).click();
 
-        // Assert product shown in cart
+        // Assert product is shown in cart
         cy.get(cartProductName).then($cartProduct => {
-          const cartProduct = $cartProduct.text();
-          expect(cartProduct).to.equal(productName);
+          expect($cartProduct.text()).to.equal(productName);
         });
 
         // Assert cart subtotal is correct
@@ -63,6 +106,7 @@ productNames.forEach(productName => {
           const cartValue = $cartValue.text().replace(valueRegex, '');
           expect(cartValue).to.equal(productValue);
         });
+        
       });
     });
 
@@ -97,7 +141,6 @@ describe(`Validate multiple products "${productNames[0]}", "${productNames[1]}" 
     });
 
     let cartProducts = [];
-
 
     // Assert products are in the cart
     cy.get(cartProductName).each(($cartProduct) => {
